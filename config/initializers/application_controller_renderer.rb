c793e1460ad483cef1563fb8a3f2bb402e851c70
Apple <apple@ApplenoMacBook-Pro.local>
2019-07-11 13:14:26 +0900

    Initial Commit Message

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '7d98cd940b13e8ffd3a988a93dfb98049817808c6747a025574c67e291277ec3801216d39cb935653f60ec973f59f87721e3b483e720093800ffbdec836ade3f'
